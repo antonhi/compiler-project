f892d70cc3cadd19be041e35a58c183aa6a3d927
antonhorvath <aih180000@utdallas.edu>
2022-10-15 16:49:02 -0500

    All productions done

--- IfEnd.java
+++ IfEnd.java
@@ -2,13 +2,17 @@ public class IfEnd extends Token {
 
     private StatementList statementList;
 
+    public IfEnd () {
+        statementList = null;
+    }
+
     public IfEnd (StatementList statementList) {
         this.statementList = statementList;
     }
 
     @Override
     public String toString(int t) {
-        return getTabs(t) + "else {\n" + statementList.toString(t+1) + getTabs(t) + "}";
+        return getTabs(t) + (statementList == null ? "" : "else {\n" + statementList.toString(t+1) + getTabs(t) + "}");
     }
     
 }
--- PrintLineList.java
+++ PrintLineList.java
@@ -2,13 +2,17 @@ public class PrintLineList extends Token {
 
     private PrintList expressions;
 
+    public PrintLineList() {
+        expressions = null;
+    }
+
     public PrintLineList (PrintList expressions) {
         this.expressions = expressions;
     }
 
     @Override
     public String toString(int t) {
-        return expressions.toString(0);
+        return expressions == null ? "" : expressions.toString(0);
     }
     
 }
